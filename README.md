7402bd80bd5d9dd9a30fff5f29abb9acbc64588a
Rikard Vestergaard <rikve590@student.liu.se>
2022-09-17 20:47:28 +0200

    Use external api to autocomplete information

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 ## Simple Python script to rename a file according to Plex naming convention
 
-The scanners of Plex will work best if content is seperated and named ***MovieName (release year).ext*** or ***/TV Shows/ShowName/Season 02/ShowName – s02e17 – Optional_Info.ext***. This script will rename a file to this format and place them in the parent-folder (or create the folder if needed).
+-The scanners of Plex will work best if content is seperated and named ***/Movies/MovieName (release year).ext*** or for TV-shows ***/TV Shows/ShowName/Season 02/ShowName – s02e17 – Optional_Info.ext***. This script will rename your file to this format and place them in the parent-folder (or create the folder if needed).
+
+- Run with ```python3 plex-rename.py```
 
 ##### TV-show
 ```
-/Band of Brothers (2001)
-    /Season 01
-        /Band of Brothers (2001) - s01e01 - Currahee.mkv
+/TV Shows
+   /Band of Brothers (2001)
+      /Season 01
+         /Band of Brothers (2001) - s01e01 - Currahee.mkv
 ```
+
 ##### Movie
 ```
 /Movies
@@ -19,16 +23,19 @@ The scanners of Plex will work best if content is seperated and named ***MovieNa
 
 ```
 
-##### Movie Example
+##### TV Show example
 
 ```
-Enter path to plex media folder (or leavy empty for default): plex/media
-Enter path to the old file: projects/movies/uglyname.mkv
+Enter path to the old file: /Users/rikard/Downloads/uglyFile.mkv 
+Enter path to plex media folder (or leave empty for default): /Users/rikard/PlexMediaFolder
 Enter format (avi, mkv etc): mkv
-Enter movie or tv-show title: Avatar
-Enter year released: 2009
-Is tv-show? (leave empty for no): 
-
-Directory 'plex/media/Avatar (2009)' created
-Movie file 'plex/media/Avatar (2009)/Avatar (2009).mkv' created
+Is tv-show? (leave empty for no): yes
+Directory '/Users/rikard/PlexMediaFolder/TV Shows' created
+Enter tv show name: the grand tour
+Enter season nr: 4
+Enter episode nr: 4
+Directory '/Users/rikard/PlexMediaFolder/Tv Shows/The Grand Tour (2016)' created
+Directory '/Users/rikard/PlexMediaFolder/Tv Shows/The Grand Tour (2016)/Season 04' created
+Moving file. Please wait...
+Tv show file '/Users/rikard/PlexMediaFolder/TV Shows/The Grand Tour (2016)/Season 04/The Grand Tour (2016) - s04e04 - Carnage a Trois.mkv' created
 ```
--- plex-rename.py
+++ plex-rename.py
@@ -1,7 +1,10 @@
-from genericpath import exists, isfile
+from genericpath import exists, isdir, isfile
 import os
 import shutil
 import sys
+import requests
+import json
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -11,23 +14,75 @@ class bcolors:
     WARNING = '\033[93m'
     FAIL = '\033[91m'
 
+
 # change this to your default plex media folder
 destination_dir = '/Volumes/share/'
 
-old_file_path = input(bcolors.OKBLUE + 'Enter path to the old file: ')
-new_file_path = input(bcolors.OKBLUE + 'Enter path to plex media folder (press enter default): ')
+# themoviedb api key
+tv_api_key = ""
+
+# omdb api key
+movie_api_key = ""
+
+# directory settings
+old_file_path = input(bcolors.OKCYAN + 'Enter path to the old file: ')
+new_file_path = input(
+    bcolors.OKCYAN + 'Enter path to plex media folder (or leave empty for default): ')
+
 if new_file_path:
     destination_dir = new_file_path
-format = input(bcolors.OKBLUE + 'Enter format (avi, mkv etc): ')
-title = input(bcolors.OKBLUE + 'Enter movie or tv-show title: ')
-year = input(bcolors.OKBLUE + 'Enter year released: ')
-is_tv_show = input(bcolors.OKBLUE + 'Is tv-show? (press enter for no): ')
 
-# extra inputs
+# format and tv-show/movie settings
+format = input(bcolors.OKCYAN + 'Enter format (avi, mkv etc): ')
+is_tv_show = input(bcolors.OKCYAN + 'Is tv-show? (leave empty for no): ')
+
 if is_tv_show:
+   # request for tv show data
+    destination_dir = os.path.join(destination_dir, 'TV Shows')
+    if not os.path.isdir(destination_dir):
+        os.mkdir(destination_dir)
+        print(bcolors.OKGREEN + "Directory '% s' created" % destination_dir)
+    tv_search_string = input(bcolors.OKCYAN + "Enter tv show name: ")
+    tmdb_request = requests.get(
+        "https://api.themoviedb.org/3/search/tv?api_key=%s&language=en-US&page=1&query=%s&include_adult=false" % (tv_api_key, tv_search_string))
+    tmdb_res = tmdb_request.text
+    parse_json = json.loads(tmdb_res)
+    if parse_json["results"]:
+        tv_show_id = parse_json["results"][0]["id"]
+        title = parse_json["results"][0]["name"]
+        year = parse_json["results"][0]["first_air_date"].split("-", 1)[0]
+
     season = input(bcolors.OKCYAN + 'Enter season nr: ')
     episode = input(bcolors.OKCYAN + 'Enter episode nr: ')
-    episode_title = input(bcolors.OKCYAN + 'Enter episode title: ')
+    tmdb_details_request = requests.get(
+        "https://api.themoviedb.org/3/tv/67557/season/%s/episode/%s?api_key=%s&language=en-US" % (season, episode, tv_api_key))
+    tmdb_details_res = tmdb_details_request.text
+    parse_tmbdb_details = json.loads(tmdb_details_res)
+    episode_title = parse_tmbdb_details["name"]
+
+else:
+    # request for movie data
+    destination_dir = os.path.join(destination_dir, "Movies")
+    if not os.path.isdir(destination_dir):
+        os.mkdir(destination_dir)
+        print(bcolors.OKGREEN + "Directory '% s' created" % destination_dir)
+    search_string = input(bcolors.OKCYAN + "Enter movie title")
+    res_api = requests.get(
+        "http://www.omdbapi.com/?apikey=%s&t=%s" % (movie_api_key, search_string))
+    res_data = res_api.text
+    parse_json = json.loads(res_data)
+    if parse_json:
+        title = parse_json["Title"]
+        year = parse_json["Year"]
+
+
+# Manually add
+if not title or not year:
+    title = input(bcolors.OKCYAN + 'Enter movie or tv-show title: ')
+    year = input(bcolors.OKCYAN + 'Enter year released: ')
+
+if not os.path.isdir(destination_dir):
+    os.mkdir(destination_dir)
 
 first_directory = title + ' ' + '(' + year + ')'
 path = os.path.join(destination_dir, first_directory)
@@ -41,7 +96,7 @@ else:
     print(bcolors.OKGREEN + "Directory '% s' created" % path)
 
 if is_tv_show:
-    ## Example of output
+    # Example of output
     # TV Shows/Band of Brothers (2001)/Season 01/Band of Brothers (2001) - s01e01 - Currahee.mkv
     s_title = "Season "
     s_nr = ""
@@ -54,25 +109,27 @@ if is_tv_show:
         s_nr = "s0%s" % season
 
     if int(episode) > 10:
-         epis_nr = "e%s" % episode
+        epis_nr = "e%s" % episode
     else:
-         epis_nr = "e0%s" % episode
-        
+        epis_nr = "e0%s" % episode
+
     season_folder_path = os.path.join(path, s_title)
     if not os.path.isdir(season_folder_path):
         os.mkdir(season_folder_path)
         print(bcolors.OKGREEN + "Directory '% s' created" % season_folder_path)
-    episode_file_name = "%s - %s%s - %s.%s" % (first_directory, s_nr, epis_nr, episode_title, format)
-    final_tv_path = os.path.join("TV Shows", season_folder_path, episode_file_name)
-    if os.path.isFile(final_tv_path):
-       print(bcolors.FAIL + "The file '% s' already exists...", final_tv_path)
-    
+    episode_file_name = "%s - %s%s - %s.%s" % (
+        first_directory, s_nr, epis_nr, episode_title, format)
+    final_tv_path = os.path.join(
+        "TV Shows", season_folder_path, episode_file_name)
+    if os.path.isfile(final_tv_path):
+        print(bcolors.FAIL + "The file '% s' already exists...", final_tv_path)
+
     print(bcolors.HEADER + "Moving file. Please wait...")
     shutil.move(old_file_path, final_tv_path)
     print(bcolors.OKGREEN + "Tv show file '% s' created" % final_tv_path)
-    
+
 else:
-    ## Example of output
+    # Example of output
     # Movies/Avatar (2009)/Avatar (2009).mkv
     movie_file_name = "%s.%s" % (first_directory, format)
     final_movie_path = os.path.join("Movies", path, movie_file_name)
@@ -83,4 +140,3 @@ else:
         print(bcolors.HEADER + "Moving file. Please wait...")
         shutil.move(old_file_path, final_movie_path)
         print(bcolors.OKGREEN + "Movie file '% s' created" % final_movie_path)
-
